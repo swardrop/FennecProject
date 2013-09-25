be12109b3f0d0b19b774b4ac7d252364b5c5f49e
jameswassink <james.wassink@gmail.com>
2013-09-25 12:06:55 +1000

    Read Description now in header
    The description and declaration is in the header; this is the file that
    all modules that call Read need to #include.

---
+++ Code/weigh/read.h
@@ -0,0 +1,16 @@
+/* Read
+ *
+ * This module obtains a value representing the strain in the beam from the
+ * A/D converter and places it in a circular buffer.
+ * It receives a pointer to the location where the value should be placed, and
+ * returns a success or fail flag. On a successful return, the weigh module will
+ * increment the buffer appropriately.
+ *
+ * Inputs:  Pointer to next value in circular buffer
+ *          Signal from beam
+ *
+ * Outputs: Integer (0-1023) representing the weight into buffer
+ *          Success flag
+ */
+
+char readSignal(int*);
