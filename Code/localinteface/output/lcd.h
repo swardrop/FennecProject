6c3bde8ad6b06a5698845afc711a88b273a583dd
jameswassink <james.wassink@gmail.com>
2013-09-27 23:09:55 +1000

    LCD stub

---
+++ Code/localinteface/output/lcd.h
@@ -0,0 +1,19 @@
+#ifndef LCD_H
+#define LCD_H
+
+/* LCD module
+ *
+ * This module takes a string and writes it to the LCD.
+ * The string must contain 2 lines of max 16 characters each, separated by a
+ * newline, and ending with a null terminator (the \n and \0 are not included in
+ * the 16 characters). Eg: "This is line one\nThis is line two\0"
+ *
+ * Inputs:  Pointer to a string to be written
+ *
+ * Outputs: String on LCD
+ *          Success/fail flag
+ */
+
+char stringToLCD(char*);
+
+#endif /*LCD_H*/
