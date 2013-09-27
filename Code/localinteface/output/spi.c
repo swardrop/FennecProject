f9a82805b4c5217fef732f15f196b32bc5811680
swardrop <stephen.wardrop@gmail.com>
2013-09-28 01:28:59 +1000

    Updated SPI

--- Code/localinteface/output/spi.c
+++ Code/localinteface/output/spi.c
@@ -1,4 +1,42 @@
+#include "spi.h"
+
+#define SPI_BUFSIZE     32
+
 typedef struct spiData {
     char CSpattern;
     char *data;
 } SPIdata;
+
+static SPIdata SPI_buffer[SPI_BUFSIZE];
+static char lead_idx, trail_idx;
+
+
+void setupSPI()
+{
+
+}
+
+void sendDataSPI(char destination, char* data)
+{
+
+}
+
+void getEEPROMstring(char *data)
+{
+
+}
+
+void getEEPROMbyte(char *data)
+{
+
+}
+
+void sendEEPROMbyte(int address, char *data)
+{
+
+}
+
+void SPIisr()
+{
+    
+}
