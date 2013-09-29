3384a7b7340ed940703672645e0a3357f36ced22
Ehome820 <2words4yayang@gmail.com>
2013-09-29 14:56:35 +1000

    Update LCDcontroler.h

--- Code/LCDcontroler.h
+++ Code/LCDcontroler.h
@@ -10,6 +10,14 @@
 #define LCD_RW     RE1  ;
 #define LCD_RS     RE0  ;
 
+#define LCD_DB7_DIR TRISD3   ;
+#define LCD_DB6_DIR TRISD2   ;
+#define LCD_DB5_DIR TRISD1   ;
+#define LCD_DB4_DIR TRISD0   ;
+#define LCD_E_DIR TRISE2   ;
+#define LCD_RS_DIR TRISE0   ; 
+#define LCD_RW_DIR TRISE1   ;
+
 
 
 void init_lcd() ;
