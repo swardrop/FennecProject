0e64652696c35715b17bb72046ee10a5bec35458
swardrop <stephen.wardrop@gmail.com>
2013-09-29 23:18:23 +1000

    Updated count

--- Code/weigh/count.c
+++ Code/weigh/count.c
@@ -1,35 +1,48 @@
 #include "../state.h"
 #include "../weigh.h"
 
-char waitForInput(char* str);
+#include <stdio.h>
+
+char* waitForInput(char* str);
 int str2num(char* input, char start_idx, char end_idx);
 
 void count(void)
 {
     int weightPerItem;
-    int weight;
-    int count;
+    int weight = 0;
+    int count = 0;
 
-    if (req_state ==  COUNT)
+    while (req_state ==  COUNT)
     {
         char input[10];
         char i;
 
-        req_state = NONE;
-        while(input[0] != '#')
-        {
-            input[0] = waitForInput("Place on scale.\nPress #");
-        }
-        weight = getWeight();
-        i = 0;
-        while(input[i] != '#')
-        {
-            input[i] = waitForInput("Enter no. items.\nPress #");
-            i++;
-        }
-        count = str2num(input, 0, i-1);
+        // Print to LCD/Terminal "Place weight\n on scale"
 
-        weightPerItem = weight/count;
+        while (!getWeight()) ; // Wait for weight to be placed on scale
+        
+        // Wait for # key press
+        waitForInput("Press #\n when finished");
+        weight = getWeight();
+        
+        if (weight == 0)
+        {
+            // Print to LCD/Terminal "Weight must be != 0"
+            continue;
+        }
+
+        while (count == 0)
+        {
+            // Get number of items in pan (formatted as string)
+            input = waitForInput("Enter no. items.\nPress #");
+            // Convert string to integer
+            sscanf(input, "%d#", &count);
+            count = str2num(input, 0, i-1);
+        }
+        // Produce conversion factor
+        weightPerItem = weight/count; // Maybe make float or *10/100/1000...?
+
+        req_state = NONE;
     }
 
     weight = getWeight();
@@ -51,7 +64,7 @@ void count(void)
     }
 }
 
-char waitForInput(char* str)
+char* waitForInput(char* str)
 {
     char input;
 
