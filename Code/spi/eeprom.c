ac15deddc8c0580a67c26213e7a876eab0d6310e
Futcha <Patty_futcher@hotmail.com>
2013-10-08 17:05:09 +1100

    Update eeprom.c

--- Code/spi/eeprom.c
+++ Code/spi/eeprom.c
@@ -1,58 +1,86 @@
-
 #include "../spi.h"
 #include "eeprom.h"
 
+
+#define READ 	0x03 /* Read instruction */
+#define WRITE	0x02 /* Write instruction */
+#define WRDI	0x04 /* Reset enable latch (don't think its needed as it should do it automatically) */
+#define WREN	0x06 /* Write enable instruction */
+#define RDSR	0x05 /* Read status register */
+#define WRSR	0x01 /* Write status register */
+
 void initialiseEEPROM(void)
 {
+    char temp;
+    sendDataSPI(EEPROM_WRITE_BYTE,RDSR);
+    sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+    sendDataSPI(EEPROM_READ_BYTE,&temp);
+    temp = temp&0x7F;
 
 }
 
 
 void getEEPROMbyte(int address)
 {
-	EEADR = address;
-	EECON1bits.EEPGD = 0;
-	EECON1bits.CFGS = 0;
-	EECON1bits.RD = 1;
-	char tempData = EEDATA;
-	return tempData;
+    char temp;
+    sendDataSPI(EEPROM_WRITE_BYTE,RDSR);
+    sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+    sendDataSPI(EEPROM_READ_BYTE,&temp);
+    if(!temp&0x02)
+    {
+        sendDataSPI(EEPROM_WRITE_BYTE, READ);
+        int addressL = (address & 0xFF);
+        int addressH = (address >> 8);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressH);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressL);
+        sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+        sendDataSPI(EEPROM_READ_BYTE,&temp);
+        return temp;
+    }
+    /*terminated by raising CS*/
+    return -1;
 }
 
-void getEEPROMstring(int address, int len)
+void getEEPROMstring(int address)
 {
-	int i;
-	for(i=0;i<len; i++)
-	{
-		getEEPROMbyte(address++);
-	}
-	return i;
-
+    char temp;
+    sendDataSPI(EEPROM_WRITE_BYTE,RDSR);
+    sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+    sendDataSPI(EEPROM_READ_BYTE,&temp);
+    if(!temp&0x02)
+    {
+        sendDataSPI(EEPROM_WRITE_BYTE, READ);
+        int addressL = (address & 0xFF);
+        int addressH = (address >> 8);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressH);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressL);
+        sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+        sendDataSPI(EEPROM_READ_STRING,&temp);
+        return temp;
+        /*terminated by raising CS*/
+    }
+    return -1;
 }
 
 void sendEEPROMbyte(int address, char *data)
 {
-    	EECON1bits.EEPGD = 0;		/*Access Data EEPROM memory*/
-	EECON1bits.CFGS = 0;		/*Access FLASH Program or Data EEPROM memory*/
-	EEADR = address;
-	EEDATA = data;
-	EECON1bits.WREN = 1;		/*Enable writes*/
-	INTCONbits.GIE = 0;		/*Disable global interrupts*/
-	EECON2 = 0x55;			/*Required*/
-	EECON2 = 0xAA;
-	EECON1bits.WR = 1; 		/*start write*/
-	INTCONbits.GIE = 1; 		/*Enable global interrupts*/
-	while(!EECON1bits.WR)		/*check data has been written, I don't know if this needs to (should not use a while loop)*/
-	PIR2bits.EEIF = 0;
-	EECON1bits.WREN = 0; 
-}
+    sendDataSPI(EEPROM_WRITE_BYTE, WREN);
+    /* set CS high and then low again after this*/
+    char temp;
+    sendDataSPI(EEPROM_WRITE_BYTE,RDSR);
+    sendDataSPI(EEPROM_WRITE_BYTE,0x00);
+    sendDataSPI(EEPROM_READ_BYTE,&temp);
 
-void sendEEPROMstring(int address, char *data, int len)
-{
-	int i;
-	for(i=0;i<len; i++)
-	{
-		sendEEPROMbyte(address++,data[i]);
-	}
-	return i;
-
+    if(temp&0x01)
+    {
+        sendDataSPI(EEPROM_WRITE_BYTE, WRITE);
+        int addressL = (address & 0xFF);
+        int addressH = (address >> 8);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressH);
+        sendDataSPI(EEPROM_WRITE_BYTE, addressL);
+        sendDataSPI(EEPROM_WRITE_BYTE,data);
+        sendDataSPI(EEPROM_WRITE_BYTE,data);
+        return data;
+    }
+    return -1;
 }
