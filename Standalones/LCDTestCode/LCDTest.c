02e3260cbce8574ef287734a2156ad91342be7a5
jameswassink <james.wassink@gmail.com>
2013-10-09 17:55:04 +1100

    Added LCD Test
    and changed .gitignore to ignore all mplab projects

---
+++ Standalones/LCDTestCode/LCDTest.c
@@ -0,0 +1,17 @@
+#include "../../Code/localinterface/output/lcd.h"
+
+void main(void)
+{
+    long int count = 0;
+    /*Write to the top line*/
+    stringToLCD("Hello World!", 1);
+    /*Write to second line*/
+    stringToLCD("PorqueNoLosDos?", 2);
+    /*Delay*/
+    while (count < 0xFFFFFFFF)
+    {
+        count++;
+    }
+    /*Overwrite top line with a shorter string*/
+    stringToLCD("Shorty");
+}
