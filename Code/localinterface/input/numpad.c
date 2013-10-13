988fa304f082900b809a3f7c85c51bb4e1ec29bd
Ehome820 <2words4yayang@gmail.com>
2013-10-13 23:40:39 +1100

    Update numpad.c

--- Code/localinterface/input/numpad.c
+++ Code/localinterface/input/numpad.c
@@ -19,22 +19,37 @@ char getNextNum()
     int temp,num;
     if((interrupt*0x01)==0x01)
     {temp=keynum&0xf0;
-     
+     if(i<16;np_buffer[i] != '\n';i++){
     switch(temp)
     {
         case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
+        case 10000000b:num=1;break;
+        case 01000000b:num=2;break;
+        case 11000000b:num=3;break;
+        case 00100000b:num=4;break;
+        case 10100000b:num=5;break;
+        case 01100000b:num=6;break;
+        case 11100000b:num=7;break;
+        case 00010000b:num=8;break;
+        case 10010000b:num=9;break;
+        case 01010000b:num=10;break;
+        case 11010000b:num=11;break;
+        case 00110000b:num=12;break;
+        case 10110000b:num=13;break;
+        case 01110000b:num=14;break;
+        case 11110000b:num=15;break;
+        default;break;
     }
+        np_buffer[i]=num;
+        return num;
+     }
+     else return -1;
+    
+    
     }
-    return -1;
+    
+   
+    
 }
 
 // The ISR captures the value and write into circular buffer.
@@ -43,7 +58,4 @@ void numpadISR()
     // Capture number
 }
 
-void initialiseNumPad(void)
-{
-    
-}
+
