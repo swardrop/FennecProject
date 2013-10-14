8ad25846b97a16f75a8b5a8d3df07bebfabb21e1
trevorganderson <trevor.g.anderson@gmail.com>
2013-10-14 11:50:14 +1100

    Revert "Update numpad.c"
    This reverts commit f3814b8aa92def5d5bde329c9ebb13135352c384.

--- Code/localinterface/input/numpad.c
+++ Code/localinterface/input/numpad.c
@@ -3,37 +3,8 @@
 char np_buffer[NP_BUFSIZE];
 static char *np_lead_ptr, *np_trail_ptr;
 
-void delay(unsigned int ms)
-{
-    unsigned char i,j;
-    for(i=0;i<ms;i++)
-    {
-        for(j=0;j<110;j++)
-    }
-    }
-    
-
-
 char getNextNum()
 {
-    int temp,num;
-    if((interrupt*0x01)==0x01)
-    {temp=keynum&0xf0;
-     
-    switch(temp)
-    {
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-        case 00000000b:num=0;break;
-    }
-    }
     return -1;
 }
 
